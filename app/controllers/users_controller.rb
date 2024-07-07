25d30053e522d10025053d9fd6528a9bd581ddac
Devpe007 <anddev007pe@gmail.com>
2024-07-07 00:28:56 -0400

    feature: user authentication method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    @user = User.find_by(username: user_params[:username])
+
+    if @user && @user.authenticate(user_params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }, status: :ok
+    else
+      render json: { error: 'Usuario ou senha invalidos' }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
